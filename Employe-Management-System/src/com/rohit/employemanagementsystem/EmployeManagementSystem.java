f5b784cd94315a3b52deaeb8e790bdd81769ef91
DesaleRohit <rohitdesale2005@gmail.com>
2025-06-18 19:21:26 +0530

    [ADDED]:Rohit - some minor modification

---
+++ Employe-Management-System/src/com/rohit/employemanagementsystem/EmployeManagementSystem.java
@@ -0,0 +1,8 @@
+package com.rohit.employemanagementsystem;
+
+public class EmployeManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("Wellcome to Employe Management System");
+
+    }
+}
